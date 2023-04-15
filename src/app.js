8630b8bc08d0d693ab004ab7f5e1d3085fba98a9
Marco <marcocarrilloosuna@gmail.com>
2023-04-15 17:25:40 -0600

    :rocket: feat: rabbitmq connection

--- src/app.js
+++ src/app.js
@@ -3,11 +3,22 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 var cors = require("cors");
 var typeorm_1 = require("typeorm");
+var amqp = require("amqplib/callback_api");
 (0, typeorm_1.createConnection)().then(function (db) {
-    var app = express();
-    app.use(cors());
-    app.use(express.json());
-    var PORT = 8001;
-    console.log("Listening to port: ".concat(PORT));
-    app.listen(PORT);
+    amqp.connect('amqps://rsbozcyr:XDoJ_MDnkPqvTlJ0WJQdMPTj2FajyVQ-@toad.rmq.cloudamqp.com/rsbozcyr', function (error0, connection) {
+        if (error0) {
+            throw error0;
+        }
+        connection.createChannel(function (error1, channel) {
+            if (error1) {
+                throw error1;
+            }
+            var app = express();
+            app.use(cors());
+            app.use(express.json());
+            var PORT = 8001;
+            console.log("Listening to port: ".concat(PORT));
+            app.listen(PORT);
+        });
+    });
 });
--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,32 @@
 import * as express from 'express';
 import * as cors from 'cors';
 import { createConnection } from 'typeorm';
-import { Product } from './entity/product';
+import * as amqp from 'amqplib/callback_api';
 
 createConnection().then(db => {
-    const app = express();
 
-    app.use(cors());
-    app.use(express.json());
+    amqp.connect('amqps://rsbozcyr:XDoJ_MDnkPqvTlJ0WJQdMPTj2FajyVQ-@toad.rmq.cloudamqp.com/rsbozcyr', (error0, connection) => {
+        if (error0) {
+            throw error0;
+        }
 
-    const PORT = 8001;
+        connection.createChannel((error1, channel) => {
+            if (error1) {
+                throw error1;
+            }
 
-    console.log(`Listening to port: ${PORT}`);
-    app.listen(PORT);
+            const app = express();
+
+            app.use(cors());
+            app.use(express.json());
+
+            const PORT = 8001;
+
+            console.log(`Listening to port: ${PORT}`);
+            app.listen(PORT);
+        });
+    
+    });
+    
 });
 
