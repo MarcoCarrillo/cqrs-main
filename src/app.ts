17a13b6b58547a33662de84106625ff057b19e23
Marco <marcocarrilloosuna@gmail.com>
2023-04-15 18:06:53 -0600

    :rocket: feat: consuming events

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,10 @@ import * as express from 'express';
 import * as cors from 'cors';
 import { createConnection } from 'typeorm';
 import * as amqp from 'amqplib/callback_api';
+import { Product } from './entity/product';
 
 createConnection().then(db => {
-
+    const productRepository = db.getMongoRepository(Product);
     amqp.connect('amqps://rsbozcyr:XDoJ_MDnkPqvTlJ0WJQdMPTj2FajyVQ-@toad.rmq.cloudamqp.com/rsbozcyr', (error0, connection) => {
         if (error0) {
             throw error0;
@@ -15,15 +16,61 @@ createConnection().then(db => {
                 throw error1;
             }
 
+            channel.assertQueue('product_created', { durable: false })
+            channel.assertQueue('product_updated', { durable: false })
+            channel.assertQueue('product_deleted', { durable: false })
+
             const app = express();
 
             app.use(cors());
             app.use(express.json());
 
+            channel.consume('product_created', async (msg) => {
+                const eventProduct: Product = JSON.parse(msg.content.toString());
+                const product = new Product();
+                product.admin_id = parseInt(eventProduct.id);
+                product.title = eventProduct.title;
+                product.image = eventProduct.image;
+                product.likes = eventProduct.likes; 
+
+                await productRepository.save(product);
+                console.log('product created');
+            }, { noAck: true });
+
+            channel.consume('product_updated', async (msg) => {
+                const eventProduct: Product = JSON.parse(msg.content.toString());
+                const product = await productRepository.findOne({ admin_id: eventProduct.id });
+                
+                product.admin_id = parseInt(eventProduct.id);
+                product.title = eventProduct.title;
+                product.image = eventProduct.image;
+                product.likes = eventProduct.likes; 
+
+                productRepository.merge(product, {
+                    title: eventProduct.title,
+                    image: eventProduct.image,
+                    likes: eventProduct.likes, 
+                });
+
+                await productRepository.save(product);
+                console.log('product updated');
+            }, { noAck: true });
+
+            channel.consume('product_deleted', async (msg) => {
+                const admin_id = parseInt(msg.content.toString());
+                
+                await productRepository.deleteOne({ admin_id });
+                console.log('product deleted');
+            });
+
             const PORT = 8001;
 
             console.log(`Listening to port: ${PORT}`);
             app.listen(PORT);
+            process.on('beforeExit', () => {
+                console.log('Closing');
+                connection.close()
+            });
         });
     
     });
